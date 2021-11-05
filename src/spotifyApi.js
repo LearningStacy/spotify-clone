9fc1d798d7e598c66d1a11ecf2098f73b4f61350
LearningStacy <stacy.saintpaul@gmail.com>
2021-11-05 10:30:51 -0400

    grab accessToken from url

--- src/spotifyApi.js
+++ src/spotifyApi.js
@@ -13,4 +13,16 @@ const scope = [
     "user-modify-playback-state",
 ];
 
+// grab accessToken from url - pulls access token after # and stop before the &
+export const getAccessToken = () =>{
+    return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item)=> {
+        let parts = item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+        return initial;
+        }, {});
+} 
+
 export const loginUrl = `${auth}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join("%20")}&response_type=token&show_dialog=true`;
