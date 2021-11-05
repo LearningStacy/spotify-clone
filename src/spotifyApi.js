6f4f04baaa437a7b7f37904eb00bdcc73a054447
LearningStacy <stacy.saintpaul@gmail.com>
2021-11-05 08:42:05 -0400

    fixed typos

--- src/spotifyApi.js
+++ src/spotifyApi.js
@@ -1,5 +1,5 @@
 // login through spotify
-export const auth = "https://accounts.spotify.com/authorize?client_id=f2d671c229d341b7bc2993a44d13cb67&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+export const auth = "https://accounts.spotify.com/authorize";
 
 const redirectUri = 'http://localhost:3000/';
 const clientId = "f2d671c229d341b7bc2993a44d13cb67";
@@ -13,4 +13,4 @@ const scope = [
     "user-modify-playback-state",
 ];
 
-export const loginUrl = `${auth}?client_id=${clientId}&redirect_uri=${redirectUri}&${scope.join("%20")}$response_type=token&show_dialog=true`;
+export const loginUrl = `${auth}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join("%20")}&response_type=token&show_dialog=true`;
