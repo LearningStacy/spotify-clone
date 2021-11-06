942a1eec1495bcb4afa9fb54b3d4e19272402e0c
LearningStacy <stacy.saintpaul@gmail.com>
2021-11-06 03:10:16 -0400

    setup area for player controls

---
+++ src/components/playerBar/PlayerBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './playerBar.scss';
+
+export default function PlayerBar() {
+    return (
+        <div className="footer">
+            <h1>I am the player bae</h1>
+        </div>
+    )
+}
