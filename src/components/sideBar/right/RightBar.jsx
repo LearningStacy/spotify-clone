878d98938bc4be02bde2434199b5633f1c561b1b
LearningStacy <stacy.saintpaul@gmail.com>
2021-11-06 03:10:45 -0400

    setup design for main (right side)

---
+++ src/components/sideBar/right/RightBar.jsx
@@ -0,0 +1,12 @@
+//this is main content side
+import React from 'react';
+import './rightBar.scss';
+
+
+export default function RightBar() {
+    return (
+        <div className="body">
+            I am in main
+        </div>
+    )
+}
