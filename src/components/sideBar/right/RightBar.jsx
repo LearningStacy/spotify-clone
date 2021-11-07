88da3d922eff5380a15b04217f213546658a8833
LearningStacy <stacy.saintpaul@gmail.com>
2021-11-07 02:39:22 -0500

    added Header component in main

--- src/components/sideBar/right/RightBar.jsx
+++ src/components/sideBar/right/RightBar.jsx
@@ -1,12 +1,13 @@
 //this is main content side
 import React from 'react';
+import Header from '../../header/Header';
 import './rightBar.scss';
 
 
-export default function RightBar() {
+export default function RightBar({spotify}) {
     return (
         <div className="body">
-            I am in main
+            <Header spotify={spotify}/>
         </div>
     )
 }
