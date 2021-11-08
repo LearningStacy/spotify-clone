bcb9f4c8c4288c7ed705481d5a49bf7465a4c32b
LearningStacy <stacy.saintpaul@gmail.com>
2021-11-08 08:17:00 -0500

    added playback code, but there are bugs
    BUG: no functionality in the any button. I double checked spelling in
    reducer/dispatch, I changed dependencies, tested the fx one by one none
    worked.

--- src/components/playerBar/PlayerBar.jsx
+++ src/components/playerBar/PlayerBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './playerBar.scss';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
@@ -8,26 +8,137 @@ import RepeatIcon from '@material-ui/icons/Repeat';
 import { Grid, Slider } from '@material-ui/core';
 import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import { useDataLayerValue } from '../../dataLayer';
+import { PauseCircleOutlineIcon } from '@material-ui/icons/PauseCircleOutline';
 
-export default function PlayerBar() {
+export default function PlayerBar({spotify}) {
+    const [{token, item, playing}, dispatch] = useDataLayerValue();
+
+    useEffect(()=> {
+        // dispatch({
+        //     type: "SET_SPOTIFY",
+        //     spotify: spotify,
+        // });
+        
+        spotify.getMyCurrentPlaybackState().then((response) => {
+            console.log(response);
+
+            dispatch({
+                type: "SET_PLAYING",
+                playing: response.is_playing,
+            });
+
+            dispatch({
+                type: "SET_ITEM",
+                item: response.item,
+            });
+        });
+    }, [spotify]);
+    
+    const skipPrevious = () => {
+        spotify.skipToPrevious();
+        spotify.getMyCurrentPlayingTrack()
+        .then((response)=> {
+            dispatch({
+                type: "SET_ITEM",
+                item: response.item,
+            });
+            dispatch({
+                type: "SET_PLAYING",
+                playing: true,
+            });
+        });
+    };
+
+    const skipNext = () => {
+        spotify.skipToNext();
+        spotify.getMyCurrentPlayingTrack()
+        .then((response) => {
+            dispatch({
+                type: "SET_ITEM",
+                item: response.item,
+            });
+            dispatch({
+                type: "SET_PLAYING",
+                playing: true,
+            });
+        });
+    };
+
+    const PlayPause = () => {
+        if(playing) {
+            spotify.pause();
+            dispatch({
+                type: "SET_PLAYING",
+                playing: false,
+            });
+        } else {
+            spotify.play();
+            dispatch({
+                type: "SET_PLAYING",
+                playing: true,
+            });
+        }
+    };
+
+    
     return (
         <div className="footer">
             {/* Song details  */}
             <div className="footer__left">
-                <img className = "footer__albumLogo" src="" alt="album cover" />
+                <img className = "footer__albumLogo" src={item?.album.images[0].url} alt={item?.name} />
+                
+                {
+                    item ? (
+                        <div className="footer__songInfo">
+                            <h4>{item.name}</h4>
+                            <p>
+                                {item.artists.map((artist)=> 
+                                    artist.name).join(", ")}
+                            </p>
+                        </div>
+                    ) : (
+                        
                 <div className="footer__songInfo">
                     <h4>No Song is Playing</h4>
-                    <p>Artist Name</p>
+                    <p>...</p>
                 </div>
+                    )
+                }
                 
             </div>
 
             {/* Player controls */}
             <div className="footer__center">
                 <ShuffleIcon className="footer__green"/>
-                <SkipPreviousIcon className="footer__icon"/>
-                <PlayCircleOutlineIcon fontSize="large" className="footer__icon"/>
-                <SkipNextIcon className="footer__icon"/>
+
+                {/* Previous Button functionality */}
+                <SkipPreviousIcon 
+                    className="footer__icon"
+                    onClick={skipNext}
+                />
+                
+                {/* Play Pause */}
+                {playing ? (
+                    <PauseCircleOutlineIcon 
+                        onClick={PlayPause}
+                        fontSize="large"
+                        className="footer__icon"
+                    />
+                    ): (
+                    <PlayCircleOutlineIcon 
+                    fontSize="large" 
+                    className="footer__icon"
+                    onClick={PlayPause}
+                    />
+                    
+                )}
+                
+                <SkipNextIcon 
+                    className="footer__icon"
+                    onClick={skipPrevious}/>
+
+                
                 <RepeatIcon className="footer__green"/>
             </div>
 
@@ -43,8 +154,9 @@ export default function PlayerBar() {
                         <VolumeDownIcon/>
                     </Grid>
 
-                    <Grid item>
-                        <Slider aria-labelledby="continuous-slider"/>
+                    <Grid item xs> 
+                        <Slider
+                            aria-labelledby="continuous-slider"/>
                     </Grid>
                 </Grid>
             </div>
